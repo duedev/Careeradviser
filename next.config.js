7e0ee5507ed8e5118416ae208a7355d4550b3c08
duedev <70658447+duedev@users.noreply.github.com>
2025-07-14 17:40:39 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   output: 'export',
-  basePath: '/career-adviser',  // Replace with your repo name
-  assetPrefix: '/career-adviser/',
+  basePath: '/Careeradviser',  // Replace with your repo name
+  assetPrefix: '/Careeradviser/',
 };
